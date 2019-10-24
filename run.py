97c000ea8b315067958e04ee417040ba2e2f0808
teerameth <teerameth.sk@gmail.com>
2019-10-24 17:01:36 +0700

    enter

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
-from app import app
-app.run(debug = True)
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def HelloWorld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
