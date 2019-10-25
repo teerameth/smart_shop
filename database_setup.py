2436b1bce59c4101db3a5b1dfcef23afc3972fd6
teerameth <teerameth.sk@gmail.com>
2019-10-26 00:47:06 +0700

    enter

--- database_setup.py
+++ database_setup.py
@@ -8,26 +8,41 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
-
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
+class Tool(Base):
+    __tablename__ = 'tool'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(200), nullable=False)
     description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
+    type = Column(String(80), nullable=False)
+    total = Column(Integer)
+    in_stock = Column(Integer)
+    picture = Column(String(200))
+    pair = Column(Integer, ForeignKey('tool.id'))
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+class Student(Base):
+    __tablename__ = 'student'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    surname = Column(String(200))
+    student_type = Column(String(50))
+    student_year = Column(Integer)
+    phone_number = Column(String(50))
+
+class ToolList(Base):
+    __tablename__ = 'tool_list'
+    id = Column(Integer, nullable=False, primary_key=True)
+    owner_id = Column(Integer, nullable=False)
+    datetime_created = Column(String(30), nullable=False) #YYYY-MM-DD HH:MI:SS
+    datetime_approved = Column(String(30))
+    datetime_returned = Column(String(30))
+    approved_status = Column(Integer)
+    returned_status = Column(Integer)
+    shared = Column(Integer)
+    shared_from_id = Column(Integer)
+    
+
+engine = create_engine('sqlite:///shop.db')
 
 
 Base.metadata.create_all(engine)
