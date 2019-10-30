612e59da76215ae2e40eaf0bb81dafa46be1e6a3
teerameth <teerameth.sk@gmail.com>
2019-10-30 16:09:26 +0700

    enter

--- gen_student.py
+++ gen_student.py
@@ -5,3 +5,6 @@ editor = Editor()
 default_password = "12345678"
 student_type = ["ปริญญาตรี", "ปริญญาโท", "ปริญญาเอก", "อื่น ๆ"]
 editor.student.create_new_student() # password, student_university_ID, name, surname, student_type, student_year, phone_number
+
+
+
