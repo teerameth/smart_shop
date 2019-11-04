c42d0780d1edf6c62f14c0dbd975f8bcc4af4fd2
Sirikorn-praew <praew22034@gmail.com>
2019-11-04 22:59:07 +0700

    enter

--- Ps.py
+++ Ps.py
@@ -1,18 +1,25 @@
 from flask import Flask,render_template,request,redirect,url_for
 import pymysql
+from flask_sqlalchemy import SQLAlchemy
+from database_setup import *
+from editor import Editor
+from gen_student import * 
+import sqlite3
+from conversion import password_encode, password_verify
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:170243809@localhost/dbshop
 # db = sqlalchemy(app)
 
-conn = pymysql.connect('localhost','root','170243809','dbshop')
+# conn = pymysql.connect('localhost','root','170243809','dbshop')
+editor = Editor()
 
 @app.route("/")
 def showData():
-    with conn:
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM list")
-        rows = cur.fetchall()
+    # with conn:
+    #     cur = conn.cursor()
+    #     cur.execute("SELECT * FROM list")
+    #     rows = cur.fetchall()
     return render_template('index.html',datas = rows)
 
 @app.route("/add")
@@ -39,6 +46,7 @@ def insert():
     return redirect(url_for('showData'))
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
-    print()
+    # app.debug = True
+    # app.run(host='0.0.0.0', port=5000)
+    st = editor.get_student_by_id("61340500068")
+    print(st.name)
