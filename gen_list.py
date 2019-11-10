8fd33ff8d1209537fcaa24d329b34dccd6feba6a
teerameth <teerameth.sk@gmail.com>
2019-11-10 14:40:02 +0700

    enter

---
+++ gen_list.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from database_setup import db, Tool_list, Student, Tool, Tool_group, Association, Order
+from editor import Editor
+editor = Editor()
+from random import randint
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+students = editor.list_all_student()
+tools = editor.list_all_tool()
+print(tools[0].name)
+for student in students:
+    print(student.name)
+    new_list = student.create_new_list()
+    new_list.add_new_tool(tools[0], 1)
+
+student = db.session.query(Student).filter_by(student_university_ID="61340500032").one()
+
+for lit in student.get_lists():
+    for order in lit.orders:
+        print(order.tool[0].name)
+new_list = student.create_new_list()
+
+#######################################
+for student in students:
+    print(student.name)
+    for lists in student.get_lists():
+        for order in lists.orders:
+            print(len(order.tool))
+            #print(order.tool[0])
