54b862f90f3076e913ceef1eb917062ecd2d0764
teerameth <teerameth.sk@gmail.com>
2019-11-29 13:44:21 +0700

    enter

--- finalProject.py
+++ finalProject.py
@@ -69,7 +69,11 @@ def editToolList(student_id, toollist_id): #"Edit tool list and go to confirm"
     status = content()
     alltool = editor.list_all_tool()
     toollist = editor.get_tool_list_by_id(toollist_id)
-    return render_template('edit_tool_list.html', student = student , status=status , alltool=alltool , toollist=toollist, toollist_id = toollist_id)
+    basket = []
+    for order in toollist.orders:
+        basket.append(order.tool.id)
+    print(basket)
+    return render_template('edit_tool_list.html', student = student , status=status , alltool=alltool , toollist=toollist, toollist_id = toollist_id, basket = basket)
 
 @app.route('/user/<int:student_id>/<int:toollist_id>/<tool>/add_tool')
 def add_tool(student_id, toollist_id,tool): #"add tool to list"
